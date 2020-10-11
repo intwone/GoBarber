cc8908e97d4d3a39c38babbb8eb6c425b010eb23
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-10-11 17:16:08 -0300

    Config typeorm metadata

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 
@@ -9,5 +10,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('🚀 Server started on port 3333!')
 });
