01cafd066c3b53471ad30f305d2564d31f8d65ab
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-10-18 15:16:32 -0300

    Added updated_at and created_at columns

--- backend/src/database/migrations/1602356980638-CreateAppointments.ts
+++ backend/src/database/migrations/1602356980638-CreateAppointments.ts
@@ -16,13 +16,21 @@ export class CreateAppointments1602356980638 implements MigrationInterface {
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          }
         ],
       }),
     );
