724e473e89fabcc4e846eeef8a7588aa29f55e01
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-10-24 16:29:46 -0300

    Replaced Error to AppError

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
 import User from '../models/Users';
 
 interface Request {
@@ -18,7 +19,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('E-mail address already in use.');
+      throw new AppError('E-mail address already in use.');
     }
 
     const hasedPassword = await hash(password, 8);
