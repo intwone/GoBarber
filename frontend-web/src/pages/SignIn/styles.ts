538942bce014d1e6e995dfd30540421a687abd6a
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-10-29 07:00:26 -0300

    Created sign in and button components

--- frontend-web/src/pages/SignIn/styles.ts
+++ frontend-web/src/pages/SignIn/styles.ts
@@ -32,39 +32,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      border-radius: 10px;
-      border: 0;
-      width: 100%;
-      height: 56px;
-      color: #312e38;
-      font-weight: 500;
-      margin-top: 24px;
-      transition: background 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-    }
-
     a {
       text-decoration: none;
       display: block;
