6e474398c4f1d6722619eac2770163c22155b75c
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-11-14 16:52:40 -0300

    Created add toast functionality

--- frontend-web/src/pages/SignIn/index.tsx
+++ frontend-web/src/pages/SignIn/index.tsx
@@ -53,7 +53,12 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
 
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description:
+            'Ocorreu um erro ao realizado o login, verifique suas credenciais',
+        });
       }
     },
     [signIn, addToast],
