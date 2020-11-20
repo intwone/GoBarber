1f4ee512cc353dab6e190cdddeb08ee5c470290a
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-11-20 20:43:03 -0300

    Added layout in SignUp component

--- gobarberapp/src/pages/SignIn/index.tsx
+++ gobarberapp/src/pages/SignIn/index.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React from 'react'
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container>
-    <Image source={logoImg}/>
+  const navigation = useNavigation();
 
-    <Title>Faça seu login</Title>
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg}/>
 
-    <Input name="email" icon="mail" placeholder="E-mail" />
+            <View>
+              <Title>Faça seu login</Title>
+            </View>
 
-    <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
 
-    <Button onPress={() => { console.log('OK') }}>Entrar</Button>
-  </Container>
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => { console.log('OK') }}>Entrar</Button>
+
+            <ForgotPassword onPress={() => { console.log('')}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
+  )
 }
 
 export default SignIn;
