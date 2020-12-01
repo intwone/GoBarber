c35b246020f79fa912cd1e3114c4231476e85fc0
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-12-01 19:54:15 -0300

    Applied Dependency Inversion Principle

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,29 +1,29 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointementsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointementsRouter.use(ensureAuthenticated);
 
-appointementsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+// appointementsRouter.get('/', async (request, response) => {
+//   const allRepository = await appointmentsRepository.find();
 
-  const allRepository = await appointmentsRepository.find();
-
-  return response.json(allRepository);
-});
+//   return response.json(allRepository);
+// });
 
 appointementsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     provider_id,
